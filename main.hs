c1536b48a23cdaeaf7a8b403d8407ed61a3a6f63
locksten <locksten@users.noreply.github.com>
2015-03-17 12:40:56 +0200

    Initial commit

---
+++ main.hs
@@ -0,0 +1,51 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+main :: IO ()
+main = do
+    args <- getArgs
+    unless (length args < 1) $ putStrLn $ readExpr $ head args
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "readExprRoot" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many $ letter <|> digit <|> symbol
+    let atom = first:rest
+    return $ case atom of
+               "#t" -> Bool True
+               "#f" -> Bool False
+               _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseString :: Parser LispVal
+parseString = do
+    _ <- char '"'
+    x <- many (noneOf "\"")
+    _ <- char '"'
+    return $ String x

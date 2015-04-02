a09afc4b87e5315a62dd8ee8d493d0fd1c6a9d69
locksten <locksten@users.noreply.github.com>
2015-04-02 15:39:48 +0300

    Add options to parse or evaluate to the executable

--- executable/src/Main.hs
+++ executable/src/Main.hs
@@ -1,8 +1,23 @@
 import SchemeParser
+import SchemeEval
 import System.Environment
 import Control.Monad
 import Control.Applicative ((<$>), (<*), (*>), (<*>))
 
 main :: IO ()
-main = getArgs >>= (\args -> unless (length args < 1) $ putStrLn $
-       init <$> unlines . map show <$> parseExpressions $ head args)
+main = do
+    args <- getArgs
+    when (length args < 2) (error usage)
+    let opt = args !! 0
+    let str = args !! 1
+    case opt of
+      "-p" -> showParse str
+      "-e" -> showEval str
+      _    -> putStrLn usage
+  where usage = "Usage: option string\noptions:\n-p  parse\n-e  evaluate"
+
+showParse :: String -> IO ()
+showParse = putStrLn . show . parseExpressions
+
+showEval :: String -> IO ()
+showEval = putStrLn . show . (map eval) . parseExpressions

5377434b77bcaa6b8bab00a7bfb219504e238665
locksten <locksten@users.noreply.github.com>
2015-04-02 15:39:18 +0300

    Add rudimentary parsing

---
+++ library/src/SchemeEval.hs
@@ -0,0 +1,12 @@
+module SchemeEval (eval) where
+import SchemeParser
+
+eval :: LispVal -> LispVal
+eval v@(String _)  = v
+eval v@(Integer _) = v
+eval v@(Float _)   = v
+eval v@(Ratio _)   = v
+eval v@(Complex _) = v
+eval v@(Bool _)    = v
+eval v@(Char _)    = v
+eval (List [Atom "quote", v]) = v

9fa231273c7ac8fc516fcc17d10c251e04b1c941
locksten <locksten@users.noreply.github.com>
2015-04-04 14:59:10 +0300

    Add LispError

---
+++ library/src/Errors.hs
@@ -0,0 +1,28 @@
+module Errors (LispError) where
+
+import SchemeParser (LispVal(..))
+
+import Text.Parsec.Error
+
+import Control.Exception
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Other String
+instance Show LispError where show = showError
+instance Exception LispError
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected
+                                        ++ " args; found values " ++ show found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                        ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+showError (Other str)                   = show str

975b4d0e6029605b257a1d1997dc891d9eb03836
locksten <locksten@users.noreply.github.com>
2015-04-10 20:30:39 +0300

    Add Environment.hs

---
+++ library/src/Lusp/Environment.hs
@@ -0,0 +1,44 @@
+module Lusp.Environment (Env
+                        ,emptyEnv
+                        ,getVar
+                        ,setVar
+                        ,defineVar) where
+
+import Lusp.LispError (LispError(UnboundVar))
+import Lusp.LispVal (LispVal(Void))
+
+import Control.Exception (throw)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+
+type Env = IORef [(String, IORef LispVal)]
+
+emptyEnv :: IO Env
+emptyEnv = newIORef []
+
+isBound :: Env -> String -> IO Bool
+isBound env var = maybe False (const True) . lookup var <$> readIORef env
+
+getVar :: Env -> String -> IO LispVal
+getVar env var = readIORef env >>= \env' ->
+    maybe (throw $ UnboundVar "Getting an unboud variable" var)
+    (readIORef) (lookup var env')
+
+setVar :: Env -> String -> LispVal -> IO LispVal
+setVar env var value = readIORef env >>= \env' ->
+    maybe (throw $ UnboundVar "Setting an unbound variable" var)
+    (flip writeIORef value) (lookup var env')
+    >> return Void
+
+defineVar :: Env -> String -> LispVal -> IO LispVal
+defineVar env var value = isBound env var >>= \alreadyDefined ->
+    if alreadyDefined
+       then setVar env var value
+       else do newValue <- newIORef value
+               env' <- readIORef env
+               writeIORef env ((var, newValue):env')
+    >> return Void
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars env bindings = readIORef env >>= extendEnv bindings >>= newIORef
+  where extendEnv bindings' env' = (++ env') <$> (mapM addBinding bindings')
+        addBinding (var, value) = (\x -> (var, x)) <$> newIORef value

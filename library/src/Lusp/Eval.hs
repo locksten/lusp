aac9693305329661c48b300a4590600b20ef630e
locksten <locksten@users.noreply.github.com>
2015-04-23 15:49:45 +0300

    Add a stack trace

--- library/src/Lusp/Eval.hs
+++ library/src/Lusp/Eval.hs
@@ -9,7 +9,7 @@ import Lusp.Environment (childEnv
 import Lusp.LispError (LispError(BadSpecialForm
                                 ,TypeMismatch
                                 ,NumArgs
-                                ,Other))
+                                ,StackTrace))
 import Lusp.LispVal (LispVal(List
                             ,DottedList
                             ,Atom
@@ -28,63 +28,71 @@ import Lusp.LispVal (LispVal(List
 import Lusp.LispValUtils (extractList)
 import Lusp.Parser (parse)
 
-import Control.Exception (throw)
+import Control.Exception (throw
+                         ,catch)
 import Control.Monad (zipWithM_)
 import Data.Maybe (isNothing)
 
 eval :: Env -> LispVal -> IO LispVal
-eval _   v@(String _)  = return v
-eval _   v@(Integer _) = return v
-eval _   v@(Real _)    = return v
-eval _   v@(Ratio _)   = return v
-eval _   v@(Complex _) = return v
-eval _   v@(Bool _)    = return v
-eval _   v@(Char _)    = return v
-eval env (Atom v)      = getVar env v
-eval _   (List [Atom "quote", v]) = return v
-eval env (List [Atom "load", String filename]) =
+eval env val = catch (eval' env val) (care val)
+  where care :: LispVal -> LispError -> IO LispVal
+        care v e = throw $ StackTrace e v
+
+eval' :: Env -> LispVal -> IO LispVal
+eval' _   v@(String _)  = return v
+eval' _   v@(Integer _) = return v
+eval' _   v@(Real _)    = return v
+eval' _   v@(Ratio _)   = return v
+eval' _   v@(Complex _) = return v
+eval' _   v@(Bool _)    = return v
+eval' _   v@(Char _)    = return v
+eval' env (Atom v)      = getVar env v
+eval' _   (List [Atom "quote", v]) = return v
+eval' env (List [Atom "load", String filename]) =
     parse <$> readFile filename >>= mapM_ (eval env) >> return Void
-eval env (List [Atom "if", predicate, consequnce, alternative]) =
+eval' env (List [Atom "if", predicate, consequnce, alternative]) =
     eval env predicate >>= \res ->
        case res of
          Bool True  -> eval env consequnce
          _          -> eval env alternative
-eval env (List [Atom "if", predicate, consequnce]) =
+eval' env (List [Atom "if", predicate, consequnce]) =
     eval env predicate >>= \res ->
        case res of
          Bool True  -> eval env consequnce
          _          -> return Void
-eval env (List [Atom "set!", Atom var, form]) = eval env form
+eval' env (List [Atom "set!", Atom var, form]) = eval env form
     >>= setVar env var
-eval env (List [Atom "define", Atom var, form]) = eval env form
+eval' env (List [Atom "define", Atom var, form]) = eval env form
     >>= defineVar env var
-eval env (List (Atom "define" : List (Atom var : params) : body)) =
+eval' env (List (Atom "define" : List (Atom var : params) : body)) =
     makeNormalFunc env params body >>= defineVar env var
-eval env (List (Atom "define" : DottedList (Atom var : params)varargs : body)) =
-    makeVarArgFunc varargs env params body >>= defineVar env var
-eval env (List (Atom "lambda" : List params : body)) =
+eval' env (List (Atom "define" : DottedList (Atom var : params)
+    varargs : body)) =
+        makeVarArgFunc varargs env params body >>= defineVar env var
+eval' env (List (Atom "lambda" : List params : body)) =
     makeNormalFunc env params body
-eval env (List (Atom "lambda" : DottedList params varargs : body)) =
+eval' env (List (Atom "lambda" : DottedList params varargs : body)) =
     makeVarArgFunc varargs env params body
-eval env (List (Atom "lambda" : varargs@(Atom _) : body)) =
+eval' env (List (Atom "lambda" : varargs@(Atom _) : body)) =
     makeVarArgFunc varargs env [] body
-eval env (List (Atom "begin" : expressions)) =
+eval' env (List (Atom "begin" : expressions)) =
     last <$> mapM (eval env) expressions
-eval env (List (Atom "let" : List bindings : body)) =
+eval' env (List (Atom "let" : List bindings : body)) =
     bindLet env bindings >>= \e -> last <$> mapM (eval e) body
-eval env (List (func : args)) = eval env func >>=
+eval' env (List (func : args)) = eval env func >>=
     (mapM (eval env) args >>=) . apply
-eval _  badForm = throw $ BadSpecialForm "Unrecognized special form" badForm
+eval' _  badForm = throw $ BadSpecialForm "Unrecognized special form" badForm
 
 bindLet :: Env -> [LispVal] -> IO Env
 bindLet env bindings = childEnv env >>= \e ->
       mapM (eval e) inits >>= zipWithM_ (defineVar e) vars >> return e
   where vars = show . head <$> bindsList
         inits = last <$> bindsList
-        bindsList = assert . extractList <$> bindings
-        assert xs = if length xs < 2
-                       then throw $ Other "strlen2"
-                       else xs
+        bindsList = toList <$> bindings
+        toList :: LispVal -> [LispVal]
+        toList xs = if length (extractList xs) /= 2
+                       then throw $ TypeMismatch "(<var> <expr>)" xs
+                       else extractList xs
 
 apply :: LispVal -> [LispVal] -> IO LispVal
 apply (PrimitiveFunc f) args = return (f args)
--- library/src/Lusp/LispError.hs
+++ library/src/Lusp/LispError.hs
@@ -13,6 +13,7 @@ data LispError = NumArgs String [LispVal]
                | UnboundVar String String
                | DivBy0
                | Other String
+               | StackTrace LispError LispVal
 instance Show LispError where show = showError
 instance Exception LispError
 
@@ -20,10 +21,17 @@ showError :: LispError -> String
 showError (UnboundVar message varname)  = message ++ ": " ++ varname
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
 showError (NotFunction message func)    = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected " ++ expected ++ " arguments" ++
-    "; found values " ++ show found
+showError (NumArgs expected found)      = "Expected " ++ expected ++
+    " arguments" ++ "; found values " ++ show found
 showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
-                                        ++ ", found " ++ show found
-showError (ParseError parseErr)         = "Parse error at " ++ show parseErr
-showError (DivBy0)                      = "Attempted division by 0"
-showError (Other str)                   = show str
+    ++ ", found " ++ show found
+showError (ParseError parseErr) = "Parse error at " ++ show parseErr
+showError (DivBy0)              = "Attempted division by 0"
+showError (Other str)           = show str
+showError e@(StackTrace _ _)    = "\n > Stack Trace:\n" ++ showStackTrace e
+
+showStackTrace :: LispError -> String
+showStackTrace (StackTrace e v) = show v ++ "\n" ++ case e of
+    x@(StackTrace _ _) -> showStackTrace x
+    x                  -> "Exception: " ++ show x
+showStackTrace _ = error "Expected StackTrace"

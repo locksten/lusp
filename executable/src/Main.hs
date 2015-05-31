df407ce8682d83f06bda45f7c9d6fa17c65ec16f
locksten <locksten@users.noreply.github.com>
2015-05-31 17:21:02 +0300

    Improve the command line options, clean up Main.hs

--- executable/src/Main.hs
+++ executable/src/Main.hs
@@ -5,6 +5,9 @@ import Lusp.Parser (parse)
 import qualified Paths (importDirs)
 import Repl (repl
             ,showEval)
+
+import Control.Monad (when)
+import Data.List (find)
 import Data.Version (showVersion)
 import System.FilePath (takeDirectory
                        ,normalise
@@ -18,41 +21,72 @@ import System.Exit (exitFailure
 
 main :: IO ()
 main = do
+    -- Run the repl if no arguments are provided
+    null <$> getArgs >>= flip when (cwdEnv >>= repl >> exitSuccess)
     opt <- safeArg 0
-    cwd <- addTrailingPathSeparator <$> getCurrentDirectory
-    let initEnv = Paths.importDirs >>= \i -> initialEnv (cwd : i) [cwd]
-    case opt of
-      "-r"        -> initEnv >>= repl
-      "-p"        -> safeArg 1 >>= showParse
-      "-e"        -> (safeArg 1 >>=) . showEval =<< initEnv
-      "-h"        -> usageAndExit
-      "--help"    -> usageAndExit
-      "-v"        -> versionAndExit
-      "--version" -> versionAndExit
-      file     -> doesFileExist file >>= \exists ->
-        if exists then do
-                  str <- readFile file
-                  args <- getArgs
-                  let fileDir = addTrailingPathSeparator $ normalise $
-                                takeDirectory file
-                  env <- Paths.importDirs >>= \i ->
-                    initialEnv (fileDir : i) (cwd : tailOrEmpty args)
-                  execute env str
-                  else putStrLn ("The file \"" ++ file ++ "\" does not exist")
-                       >> exitFailure
-  where safeArg n = (length <$> getArgs) >>= \len ->
-          if len < n + 1 then usageAndExit
-                         else (!! n) <$> getArgs
-        tailOrEmpty xs = if null xs then [] else tail xs
-        versionAndExit = putStrLn ("lusp " ++ showVersion version)
-            >> exitSuccess
-        usageAndExit = putStrLn "Usage: \n\
-      \ filename args  execute a file\n\
-      \-r              start the repl\n\
-      \-e \"expr\"       evaluate an expression\n\
-      \-p \"expr\"       parse an expression\n\
-      \-v --version    print the version and exit\n\
-      \-h --help       print help and exit" >> exitSuccess
+    case findOption opt of
+      -- If the argument is not an option, treat it as a filename
+      Nothing -> executeFile opt
+      Just f  -> f
+
+-- | Executes the code contained within the file
+executeFile :: FilePath -> IO ()
+executeFile file = doesFileExist file >>= \exists ->
+  if exists then do
+        str <- readFile file
+        args <- getArgs
+        let fileDir = addTrailingPathSeparator $ normalise $ takeDirectory file
+        env <- Paths.importDirs >>= \i ->
+          addTrailingPathSeparator <$> getCurrentDirectory >>= \d ->
+          initialEnv (fileDir : i) (d : tailOrEmpty args)
+        execute env str
+    else putStrLn ("The file \"" ++ file ++ "\" does not exist") >> exitFailure
+  where tailOrEmpty xs = if null xs
+                            then []
+                            else tail xs
+
+-- | Returns the n'th commandline argument.
+-- If that's not possible, printsHelp and exits
+safeArg :: Int -> IO String
+safeArg n = (length <$> getArgs) >>= \len ->
+    if len < n + 1 then printHelp >> exitSuccess
+                   else (!! n) <$> getArgs
+
+-- | An environment with no other commandline arguments except the cwd
+cwdEnv :: IO Env
+cwdEnv = addTrailingPathSeparator <$> getCurrentDirectory >>= \d ->
+    Paths.importDirs >>= \i -> initialEnv (d : i) [d]
+
+-- | Prints the version of the program
+printVersion :: IO ()
+printVersion = putStrLn ("lusp " ++ showVersion version)
+
+-- | Prints the help
+printHelp :: IO ()
+printHelp = putStrLn "Usage: lusp [option]\n\
+      \<<nothing>>           start the repl\n\
+      \filename [args]       execute a file\n\
+      \-e --evaluate \"expr\"  evaluate an expression\n\
+      \-p --parse    \"expr\"  parse an expression\n\
+      \-v --version          print the version\n\
+      \-h --help             print this help"
+
+-- | A list of cammandline options with corresponding IO actions
+options :: [([String], IO ())]
+options = [(["--help"
+            ,"-h"], printHelp)
+          ,(["--version"
+            ,"-v"], printVersion)
+          ,(["--evaluate"
+            ,"-e"], (safeArg 1 >>=) . showEval =<< cwdEnv)
+          ,(["--parse"
+            ,"-p"], safeArg 1 >>= showParse)]
+
+-- | Finds the commandline action corresponding to the option string
+findOption :: String -> Maybe (IO ())
+findOption str = case find (elem str . fst) options of
+                   Nothing -> Nothing
+                   Just x  -> Just $ snd x
 
 -- | Parses and prints the code
 showParse :: String -> IO ()
